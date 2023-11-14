062fb8d5735b0be90cefd7bbcc612aec1bda651a
Jhorman2003 <131841197+Jhorman2003@users.noreply.github.com>
2023-11-13 21:38:16 -0500

    Validate

--- python/src/auth/gateway/server.py
+++ python/src/auth/gateway/server.py
@@ -10,3 +10,21 @@ server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
 mongo = PyMongo(server)
 
+fs = gridfs.GridFS(mongo.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
+
+
+@server.route("/upload", methods=["POST"])
+def upload():
+    access, err = validate.token(request)
